36a2dd53d11f6f43adf37818328a5bfaca9ef2d2
Rikuklane <richard.kuklane@ut.ee>
2022-10-16 17:19:41 +0300

    Create unit types #32

---
+++ Assets/Scripts/Data/CardData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData : ScriptableObject
+{
+    public string title;
+    public string description;
+    public int cost;
+}
