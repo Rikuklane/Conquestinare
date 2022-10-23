5a8ccea375cc6d9d67f9dda33b2314fe6b12a4d7
Rikuklane <richard.kuklane@ut.ee>
2022-10-23 11:29:02 +0300

    Update changing of states

--- Assets/Scripts/Turns/TurnManager.cs
+++ Assets/Scripts/Turns/TurnManager.cs
@@ -2,23 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Turns
 {
-    public abstract class TurnManager: MonoBehaviour
+    public class TurnManager: MonoBehaviour
     {
-        private AbstractTurnState _currentState;
-        private Player _currentPlayer;
-        private Player[] _players = { new("Player 1"), new("Player 2"), new("Player 3") };
+        public Button nextTurnButton;
+        
         public readonly PlayerStartTurn PlayerStartTurn = new();
         public readonly ReceiveUnitsTurn ReceiveUnitsTurn = new();
         public readonly MarketTurn MarketTurn = new();
         public readonly PlaceUnitsTurn PlaceUnitsTurn = new();
         public readonly BattleTurn BattleTurn = new();
         public readonly ReorganizeTurn ReorganizeTurn = new();
+        
+        private AbstractTurnState _currentState;
+        private Player _currentPlayer;
+        private Player[] _players = { new("Player 1"), new("Player 2"), new("Player 3") };
+
+        private void Awake()
+        {
+            nextTurnButton.onClick.AddListener(TriggerEndState);
+        }
 
         private void Start()
         {
+            _currentPlayer = _players[0];
             SwitchTurnState(PlayerStartTurn);
         }
 
@@ -30,12 +40,17 @@ namespace Turns
 
         public void SwitchPlayerTurn()
         {
-            var index = Array.IndexOf(_players, _currentPlayer);
-            if (index + 1 == _players.Length)
+            var nextIndex = Array.IndexOf(_players, _currentPlayer) + 1;
+            if (nextIndex == _players.Length)
             {
-                index = 0;
+                nextIndex = 0;
             }
-            _currentPlayer = _players[index];
+            _currentPlayer = _players[nextIndex];
+        }
+
+        public void TriggerEndState()
+        {
+            StartCoroutine(_currentState.EndState(this, _currentPlayer));
         }
     }
 }
