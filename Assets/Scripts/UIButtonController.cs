cb153c31a0d8e86281c953d36b88662db598edb1
Rikuklane <richard.kuklane@ut.ee>
2022-11-11 15:49:06 +0200

    Try to fix sounds

--- Assets/Scripts/UIButtonController.cs
+++ Assets/Scripts/UIButtonController.cs
@@ -9,26 +9,15 @@ public class UIButtonController : MonoBehaviour
     public AudioClipGroup clickUIButton;
     public AudioClipGroup hoverUIButton;
 
-    private Button _button;
-
-    private void Awake()
-    {
-        _button = GetComponent<Button>();
-        _button.onClick.AddListener(PlayClickUIButton);
-    }
-
-    private void PlayHoverUIButton()
-    {
-        hoverUIButton.Play();
-    }
-    
-    private void PlayClickUIButton()
-    {
-        clickUIButton.Play();
-    }
-
     private void OnMouseEnter()
     {
-        PlayHoverUIButton();
+        Debug.Log("Mouse entered!");
+        hoverUIButton.Play();
+    }
+
+    public void PlayOnClick()
+    {
+        Debug.Log("Button clicked");
+        clickUIButton.Play();
     }
 }
