d9dc99df2a1242507fcfcdeda0093bd5b3e5e3e6
markussulg <66622501+markussulg@users.noreply.github.com>
2022-11-21 10:19:39 +0200

    Player color and gold gain (#69)

--- Assets/Scripts/Turns/ReceiveUnitsTurn.cs
+++ Assets/Scripts/Turns/ReceiveUnitsTurn.cs
@@ -9,6 +9,7 @@ namespace Turns
     
         public override IEnumerator EnterState(TurnManager turnManager, Player player)
         {
+            TurnManager.Instance.goldGainText.gameObject.SetActive(true);
             AttackLogic.Instance.canHover = false;
             SetNextButtonActive(turnManager, false);
             Events.DisplayUnitSelection(3);
@@ -17,6 +18,7 @@ namespace Turns
 
         public override IEnumerator EndState(TurnManager turnManager, Player player)
         {
+            TurnManager.Instance.goldGainText.gameObject.SetActive(false);
             CardSelector.Instance.SetActive(false);
             SetNextButtonActive(turnManager, true);
             turnManager.SwitchTurnState(turnManager.MarketTurn);
