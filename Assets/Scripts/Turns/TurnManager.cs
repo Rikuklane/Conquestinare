c5358f223889faf6593ef9b3135e0bfde2650fa4
Rikuklane <richard.kuklane@ut.ee>
2022-11-21 10:46:41 +0200

    Update audio

--- Assets/Scripts/Turns/TurnManager.cs
+++ Assets/Scripts/Turns/TurnManager.cs
@@ -103,6 +103,7 @@ namespace Turns
 
         public void TriggerEndState()
         {
+            AudioController.Instance.clickUIButton.Play();
             StartCoroutine(_currentState.EndState(this, GetCurrentPlayer()));
         }
 
