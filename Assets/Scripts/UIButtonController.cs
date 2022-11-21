7224c5c7f3cb47faa37d07361e41528dfce62cc0
markussulg <66622501+markussulg@users.noreply.github.com>
2022-11-21 10:30:24 +0200

    Menu + fix (#70)

--- Assets/Scripts/UIButtonController.cs
+++ Assets/Scripts/UIButtonController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIButtonController : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class UIButtonController : MonoBehaviour
     public void PlayOnHover()
     {
         AudioController.Instance.hoverUIButton.Play();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
